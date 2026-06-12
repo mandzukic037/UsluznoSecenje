d8d77ceb87fa6bf3e9ff6350c206901e9ff240c3
Aleksa Mandžukić <aleksamandzukic037@gmail.com>
2026-06-12 03:29:11 +0200

    production build fix

--- frontend/app/src/environments/environment.ts
+++ frontend/app/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080'
+    // production: false,
+    // apiUrl: 'http://localhost:8080'
     
-    //   production: true,
-    //   apiUrl: 'https://api.usluznosecenje.rs' 
+      production: true,
+      apiUrl: 'https://usluznosecenje-production.up.railway.app'
 };
